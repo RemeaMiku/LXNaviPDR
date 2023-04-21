b4bee9e62c364647cd2e7880d961107e7f71a485
RemeaMiku <73517033+RemeaMiku@users.noreply.github.com>
2023-04-21 11:32:44 +0800

    添加PDR项目

---
+++ LXNavi.Core/DeadReckoningMeasurement.cs
@@ -0,0 +1,25 @@
+﻿using NaviSharp;
+
+namespace LXNavi.Core;
+
+public class DeadReckoningMeasurement
+{
+    #region Public Constructors
+
+    public DeadReckoningMeasurement(DateTimeOffset timeStamp, double acceleration, EulerAngle eulerAngle)
+    {
+        TimeStamp = timeStamp;
+        Acceleration = acceleration;
+        EulerAngle = eulerAngle;
+    }
+
+    #endregion Public Constructors
+
+    #region Public Properties
+
+    public DateTimeOffset TimeStamp { get; init; }
+    public double Acceleration { get; init; }
+    public EulerAngle EulerAngle { get; init; }
+
+    #endregion Public Properties
+}
